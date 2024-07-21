c7768ee46bf327f6746139dcffdd1fdad2d2258b
ravi-dhyani8881 <ravi.dhyani@mulesoft.com>
2024-07-21 16:57:47 +0000

    Automated commit by Docker container

--- Rustloc/src/main/java/com/spring/graph/QueryResolver/GenericQueryResolver.java
+++ Rustloc/src/main/java/com/spring/graph/QueryResolver/GenericQueryResolver.java
@@ -17,18 +17,18 @@ public class GenericQueryResolver {
 	@Autowired
 	RestTemplate restTemplate;
 		
-			public ravimeResponseTemplate  findravimeByQuery(String query, String start,  String rows, String filterField,  String filterQuery,  String sort, String  advanceField,  String advanceQuery,  String advance, String field) {	
+			public ravimemResponseTemplate  findravimemByQuery(String query, String start,  String rows, String filterField,  String filterQuery,  String sort, String  advanceField,  String advanceQuery,  String advance, String field) {	
 				
 			HttpHeaders headers = new HttpHeaders();
 		    headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		    HttpEntity <String> entity = new HttpEntity<String>(headers);
 		    
 			String url="http://localhost:8081/"+field+"/findByQuery?query="+query+"&start="+start+"&rows="+rows+"&filterField="+filterField+"&filterQuery="+filterQuery+"&sort"+sort+"&advanceField="+advanceField+"&advanceQuery="+advanceQuery+"&advance="+advance;
-		    ParameterizedTypeReference<ravimeResponseTemplate> responseType =
-	                new ParameterizedTypeReference<ravimeResponseTemplate>() {};
+		    ParameterizedTypeReference<ravimemResponseTemplate> responseType =
+	                new ParameterizedTypeReference<ravimemResponseTemplate>() {};
 
 			var response2=restTemplate.exchange(url,HttpMethod.GET, entity, responseType).getBody();
-			return (ravimeResponseTemplate) response2;
+			return (ravimemResponseTemplate) response2;
 		}
 			public ravime2ResponseTemplate  findravime2ByQuery(String query, String start,  String rows, String filterField,  String filterQuery,  String sort, String  advanceField,  String advanceQuery,  String advance, String field) {	
 				
