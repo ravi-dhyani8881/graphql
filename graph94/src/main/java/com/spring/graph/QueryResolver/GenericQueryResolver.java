cc5a209673514b87042e3b1e76a2e0bc0eca443d
ravi-dhyani8881 <ravi.dhyani@mulesoft.com>
2025-07-15 17:09:03 +0000

    Automated commit by Docker container

---
+++ graph94/src/main/java/com/spring/graph/QueryResolver/GenericQueryResolver.java
@@ -0,0 +1,35 @@
+package com.spring.graph.QueryResolver;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class GenericQueryResolver {
+
+	@Autowired
+	RestTemplate restTemplate;
+		
+
+		public static String toPascalCase(String input) {
+			    String[] parts = input.split("_");
+			    StringBuilder pascalCase = new StringBuilder();
+
+			    for (String part : parts) {
+			        if (part.isEmpty()) continue;
+			        pascalCase.append(Character.toUpperCase(part.charAt(0)));
+			        if (part.length() > 1) {
+			            pascalCase.append(part.substring(1).toLowerCase());
+			        }
+			    }
+
+			    return pascalCase.toString();
+			}
+}
