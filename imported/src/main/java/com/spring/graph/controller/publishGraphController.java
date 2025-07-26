e5b0926cd98ff197b675b16d47d83909d677e644
ravi-dhyani8881 <ravi.dhyani@mulesoft.com>
2025-07-26 06:35:04 +0000

    Automated commit by Docker container

---
+++ imported/src/main/java/com/spring/graph/controller/publishGraphController.java
@@ -0,0 +1,84 @@
+package com.spring.graph.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import com.spring.graph.utils.QueryUtils;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import com.spring.graph.QueryResolver.publishGraphResponseTemplate;
+import com.spring.graph.QueryResolver.GenericQueryResolver;
+import com.spring.graph.Model.RecordspublishGraph;
+  import com.spring.graph.QueryResolver.organizationResponseTemplate;
+  import com.spring.graph.QueryResolver.environmentResponseTemplate;
+  import com.spring.graph.QueryResolver.subOrganizationResponseTemplate;
+
+
+@Controller
+public class publishGraphController {
+
+	@Autowired
+	GenericQueryResolver genericQueryResolver;
+	
+	@QueryMapping
+    public publishGraphResponseTemplate findpublishGraphByQuery(@Argument String query, @Argument String start,@Argument String rows, @Argument String filterField, @Argument String filterQuery,
+    							  @Argument String sort,@Argument String advanceField,@Argument String advanceQuery,@Argument String advance) {
+		return  genericQueryResolver.findpublishGraphByQuery(query, start, rows, filterField, filterQuery, sort, advanceField, advanceQuery, advance);	
+    }
+	  
+  @SchemaMapping
+  public organizationResponseTemplate findorganizationByQuery(
+    RecordspublishGraph content,
+    @Argument String query,
+    @Argument String start,
+    @Argument String rows,
+    @Argument String filterField,
+    @Argument String filterQuery,
+    @Argument String sort,
+    @Argument String advanceField,
+    @Argument String advanceQuery,
+    @Argument String advance
+    
+  ) throws Exception {
+    String query2 = QueryUtils.replaceTokens(query, content);  
+    return genericQueryResolver.findorganizationByQuery(query2, start, rows, filterField, filterQuery, sort, advanceField, advanceQuery, advance);
+  }
+  @SchemaMapping
+  public environmentResponseTemplate findenvironmentByQuery(
+    RecordspublishGraph content,
+    @Argument String query,
+    @Argument String start,
+    @Argument String rows,
+    @Argument String filterField,
+    @Argument String filterQuery,
+    @Argument String sort,
+    @Argument String advanceField,
+    @Argument String advanceQuery,
+    @Argument String advance
+    
+  ) throws Exception {
+    String query2 = QueryUtils.replaceTokens(query, content);  
+    return genericQueryResolver.findenvironmentByQuery(query2, start, rows, filterField, filterQuery, sort, advanceField, advanceQuery, advance);
+  }
+  @SchemaMapping
+  public subOrganizationResponseTemplate findsubOrganizationByQuery(
+    RecordspublishGraph content,
+    @Argument String query,
+    @Argument String start,
+    @Argument String rows,
+    @Argument String filterField,
+    @Argument String filterQuery,
+    @Argument String sort,
+    @Argument String advanceField,
+    @Argument String advanceQuery,
+    @Argument String advance
+    
+  ) throws Exception {
+    String query2 = QueryUtils.replaceTokens(query, content);  
+    return genericQueryResolver.findsubOrganizationByQuery(query2, start, rows, filterField, filterQuery, sort, advanceField, advanceQuery, advance);
+  }
+
+
+	
+}
