262b0c5aebb474f688bbda3e9a001d757af83048
ravi-dhyani8881 <ravi.dhyani@mulesoft.com>
2025-08-06 13:24:04 +0000

    Automated commit by Docker container

---
+++ cycle/src/main/java/com/spring/graph/QueryResolver/GenericQueryResolver.java
@@ -0,0 +1,61 @@
+package com.spring.graph.QueryResolver;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class GenericQueryResolver {
+
+	@Autowired
+	RestTemplate restTemplate;
+		
+			public cycleResponseTemplate  findcycleByQuery(String query, String start,  String rows, String filterField,  String filterQuery,  String sort, String  advanceField,  String advanceQuery,  String advance) {	
+				
+			HttpHeaders headers = new HttpHeaders();
+		    headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		    HttpEntity <String> entity = new HttpEntity<String>(headers);
+		    
+			String url="http://rest-service-475e67f9-7b47-465b-a109-47ff47c41ee6:80/"+toPascalCase("cycle")+"/findByQuery?query="+query+"&start="+start+"&rows="+rows+"&filterField="+filterField+"&filterQuery="+filterQuery+"&sort="+sort+"&advanceField="+advanceField+"&advanceQuery="+advanceQuery+"&advance="+advance;
+		    ParameterizedTypeReference<cycleResponseTemplate> responseType =
+	                new ParameterizedTypeReference<cycleResponseTemplate>() {};
+
+			var response2=restTemplate.exchange(url,HttpMethod.GET, entity, responseType).getBody();
+			return (cycleResponseTemplate) response2;
+		}
+			public partsResponseTemplate  findpartsByQuery(String query, String start,  String rows, String filterField,  String filterQuery,  String sort, String  advanceField,  String advanceQuery,  String advance) {	
+				
+			HttpHeaders headers = new HttpHeaders();
+		    headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		    HttpEntity <String> entity = new HttpEntity<String>(headers);
+		    
+			String url="http://rest-service-475e67f9-7b47-465b-a109-47ff47c41ee6:80/"+toPascalCase("parts")+"/findByQuery?query="+query+"&start="+start+"&rows="+rows+"&filterField="+filterField+"&filterQuery="+filterQuery+"&sort="+sort+"&advanceField="+advanceField+"&advanceQuery="+advanceQuery+"&advance="+advance;
+		    ParameterizedTypeReference<partsResponseTemplate> responseType =
+	                new ParameterizedTypeReference<partsResponseTemplate>() {};
+
+			var response2=restTemplate.exchange(url,HttpMethod.GET, entity, responseType).getBody();
+			return (partsResponseTemplate) response2;
+		}
+
+		public static String toPascalCase(String input) {
+			    String[] parts = input.split("_");
+			    StringBuilder pascalCase = new StringBuilder();
+
+			    for (String part : parts) {
+			        if (part.isEmpty()) continue;
+			        pascalCase.append(Character.toUpperCase(part.charAt(0)));
+			        if (part.length() > 1) {
+			            pascalCase.append(part.substring(1).toLowerCase());
+			        }
+			    }
+
+			    return pascalCase.toString();
+			}
+}
---
+++ cycle/src/main/java/com/spring/graph/controller/cycleController.java
@@ -0,0 +1,48 @@
+package com.spring.graph.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import com.spring.graph.utils.QueryUtils;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import com.spring.graph.QueryResolver.cycleResponseTemplate;
+import com.spring.graph.QueryResolver.GenericQueryResolver;
+import com.spring.graph.Model.Recordscycle;
+  import com.spring.graph.QueryResolver.partsResponseTemplate;
+
+
+@Controller
+public class cycleController {
+
+	@Autowired
+	GenericQueryResolver genericQueryResolver;
+	
+	@QueryMapping
+    public cycleResponseTemplate findcycleByQuery(@Argument String query, @Argument String start,@Argument String rows, @Argument String filterField, @Argument String filterQuery,
+    							  @Argument String sort,@Argument String advanceField,@Argument String advanceQuery,@Argument String advance) {
+		return  genericQueryResolver.findcycleByQuery(query, start, rows, filterField, filterQuery, sort, advanceField, advanceQuery, advance);	
+    }
+	  
+  @SchemaMapping
+  public partsResponseTemplate findpartsByQuery(
+    Recordscycle content,
+    @Argument String query,
+    @Argument String start,
+    @Argument String rows,
+    @Argument String filterField,
+    @Argument String filterQuery,
+    @Argument String sort,
+    @Argument String advanceField,
+    @Argument String advanceQuery,
+    @Argument String advance
+    
+  ) throws Exception {
+    String query2 = QueryUtils.replaceTokens(query, content);  
+    return genericQueryResolver.findpartsByQuery(query2, start, rows, filterField, filterQuery, sort, advanceField, advanceQuery, advance);
+  }
+
+
+	
+}
