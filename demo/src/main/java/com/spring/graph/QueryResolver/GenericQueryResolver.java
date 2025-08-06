95b0153cc7d030e1aad28763c731c3fc5807bbaf
ravi-dhyani8881 <ravi.dhyani@mulesoft.com>
2025-08-06 17:07:55 +0000

    Automated commit by Docker container

---
+++ demo/src/main/java/com/spring/graph/QueryResolver/GenericQueryResolver.java
@@ -0,0 +1,61 @@
+package com.spring.graph.QueryResolver;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class GenericQueryResolver {
+
+	@Autowired
+	RestTemplate restTemplate;
+		
+			public rocketResponseTemplate  findrocketByQuery(String query, String start,  String rows, String filterField,  String filterQuery,  String sort, String  advanceField,  String advanceQuery,  String advance) {	
+				
+			HttpHeaders headers = new HttpHeaders();
+		    headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		    HttpEntity <String> entity = new HttpEntity<String>(headers);
+		    
+			String url="http://rest-service-c16c04a0-18d3-41bf-bb07-79efaa39462d:80/"+toPascalCase("rocket")+"/findByQuery?query="+query+"&start="+start+"&rows="+rows+"&filterField="+filterField+"&filterQuery="+filterQuery+"&sort="+sort+"&advanceField="+advanceField+"&advanceQuery="+advanceQuery+"&advance="+advance;
+		    ParameterizedTypeReference<rocketResponseTemplate> responseType =
+	                new ParameterizedTypeReference<rocketResponseTemplate>() {};
+
+			var response2=restTemplate.exchange(url,HttpMethod.GET, entity, responseType).getBody();
+			return (rocketResponseTemplate) response2;
+		}
+			public engineResponseTemplate  findengineByQuery(String query, String start,  String rows, String filterField,  String filterQuery,  String sort, String  advanceField,  String advanceQuery,  String advance) {	
+				
+			HttpHeaders headers = new HttpHeaders();
+		    headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		    HttpEntity <String> entity = new HttpEntity<String>(headers);
+		    
+			String url="http://rest-service-c16c04a0-18d3-41bf-bb07-79efaa39462d:80/"+toPascalCase("engine")+"/findByQuery?query="+query+"&start="+start+"&rows="+rows+"&filterField="+filterField+"&filterQuery="+filterQuery+"&sort="+sort+"&advanceField="+advanceField+"&advanceQuery="+advanceQuery+"&advance="+advance;
+		    ParameterizedTypeReference<engineResponseTemplate> responseType =
+	                new ParameterizedTypeReference<engineResponseTemplate>() {};
+
+			var response2=restTemplate.exchange(url,HttpMethod.GET, entity, responseType).getBody();
+			return (engineResponseTemplate) response2;
+		}
+
+		public static String toPascalCase(String input) {
+			    String[] parts = input.split("_");
+			    StringBuilder pascalCase = new StringBuilder();
+
+			    for (String part : parts) {
+			        if (part.isEmpty()) continue;
+			        pascalCase.append(Character.toUpperCase(part.charAt(0)));
+			        if (part.length() > 1) {
+			            pascalCase.append(part.substring(1).toLowerCase());
+			        }
+			    }
+
+			    return pascalCase.toString();
+			}
+}
