2e4d07f2cd93d7a5047f7b1fdfd44cda86c7591f
ravi-dhyani8881 <ravi.dhyani@mulesoft.com>
2025-10-17 05:35:07 +0000

    Automated commit by Docker container

---
+++ FEBE6/src/main/java/com/spring/graph/Model/Recordsgraph.java
@@ -0,0 +1,11 @@
+package com.spring.graph.Model;
+
+import org.apache.solr.common.SolrDocument;
+
+public class Recordsgraph extends SolrDocument {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+}
