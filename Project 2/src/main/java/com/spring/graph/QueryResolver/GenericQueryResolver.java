a92926be5ef1e902629ce5ea37bf10feaa30ab79
ravi-dhyani8881 <ravi.dhyani@mulesoft.com>
2026-01-26 17:20:53 +0000

    Automated commit by Docker container

--- Project 2/src/main/java/com/spring/graph/QueryResolver/GenericQueryResolver.java
+++ Project 2/src/main/java/com/spring/graph/QueryResolver/GenericQueryResolver.java
@@ -47,7 +47,7 @@ public class GenericQueryResolver {
 		            + "&advanceQuery=" + advanceQuery
 		            + "&advance=" + advance;				
   
-		//	String url="http://rest-service-a44cc2ed-4fbb-4a74-86a9-2296e99744b6:80/"+toPascalCase("users")+"/findByQuery?query="+query+"&start="+start+"&rows="+rows+"&filterField="+filterField+"&filterQuery="+filterQuery+"&sort="+sort+"&advanceField="+advanceField+"&advanceQuery="+advanceQuery+"&advance="+advance;
+		//	String url="http://rest-service-ef22f54d-c0f3-4a32-b229-af7ed9dd6796:80/"+toPascalCase("users")+"/findByQuery?query="+query+"&start="+start+"&rows="+rows+"&filterField="+filterField+"&filterQuery="+filterQuery+"&sort="+sort+"&advanceField="+advanceField+"&advanceQuery="+advanceQuery+"&advance="+advance;
 		    ParameterizedTypeReference<usersResponseTemplate> responseType =
 	                new ParameterizedTypeReference<usersResponseTemplate>() {};
 
