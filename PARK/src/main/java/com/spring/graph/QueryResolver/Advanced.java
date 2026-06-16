9f1e4bed65514a30d35ee798358b107164fa6eb8
ravi-dhyani8881 <ravi.dhyani@mulesoft.com>
2026-06-16 15:47:18 +0000

    Automated commit by Docker container

---
+++ PARK/src/main/java/com/spring/graph/QueryResolver/Advanced.java
@@ -0,0 +1,20 @@
+package com.spring.graph.QueryResolver;
+
+import java.util.ArrayList;
+
+public class Advanced{
+	public String fieldName;
+    public ArrayList<Object> values;
+	public String getFieldName() {
+		return fieldName;
+	}
+	public void setFieldName(String fieldName) {
+		this.fieldName = fieldName;
+	}
+	public ArrayList<Object> getValues() {
+		return values;
+	}
+	public void setValues(ArrayList<Object> values) {
+		this.values = values;
+	}   
+}
---
+++ PARK/src/main/java/com/spring/graph/QueryResolver/usersResponseTemplate.java
@@ -0,0 +1,47 @@
+package com.spring.graph.QueryResolver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.spring.graph.Model.Recordsusers;
+
+
+public class usersResponseTemplate {
+
+	
+	
+	@JsonProperty("pagination")
+	private Pagination pagination;
+
+	@JsonProperty("data")
+    private List<Recordsusers> data;
+    
+	@JsonProperty("advanced")
+    public ArrayList<Advanced> advanced;
+
+	
+	public Pagination getPagination() {
+		return pagination;
+	}
+
+	public void setPagination(Pagination pagination) {
+		this.pagination = pagination;
+	}
+
+	public List<Recordsusers> getData() {
+		return data;
+	}
+
+	public void setData(List<Recordsusers> data) {
+		this.data = data;
+	}
+
+	public ArrayList<Advanced> getAdvanced() {
+		return advanced;
+	}
+
+	public void setAdvanced(ArrayList<Advanced> advanced) {
+		this.advanced = advanced;
+	}
+}
